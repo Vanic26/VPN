61ef72bd99b738c28cba1b247355e9c23ab47a08
Vanic <jonny90.appleseed@mail.ru>
2026-07-31 22:50:23 +0630

    Update Private_2.py

--- src/Private_2.py
+++ src/Private_2.py
@@ -1679,6 +1679,8 @@ def main():
             print(yaml.dump(n, allow_unicode=True, sort_keys=False))
         # ---------------- Convert to YAML ----------------
         proxies_yaml_block = yaml.dump(info_ordered_dicts, allow_unicode=True, default_flow_style=False, sort_keys=False)    #If multiple lines format is needed, Delete Line by line YAML proxies output format code block, proxies_yaml_block = yaml.dump(info_ordered_dicts, allow_unicode=True, default_flow_style=False, sort_keys=False)
+        print("\n[DEBUG YAML STRING CHECK]")
+        print(proxies_yaml_block[:3000])
         proxy_names_block = "\n".join([f"      - {unquote(p['name'])}" for p in info_ordered_dicts])
 
         # ---------------- Replace placeholders ----------------
